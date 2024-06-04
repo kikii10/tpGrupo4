0584ba90fe04744e39eb377d541ad72e86bf5a91
LeanAranda <arandaleandro2003@gmail.com>
2024-06-03 22:18:44 -0300

    fix maven

--- src/test/java/com/tpGrupo4/TpGrupo4ApplicationTests.java
+++ src/test/java/com/tpGrupo4/TpGrupo4ApplicationTests.java
@@ -1,8 +1,11 @@
 package com.tpGrupo4;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootConfiguration
 @SpringBootTest
 class TpGrupo4ApplicationTests {
 
