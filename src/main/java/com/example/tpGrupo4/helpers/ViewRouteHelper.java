95642a47ab76c9ad319da30e0b40a4b5dfa0f78a
Ignacio szymborski <ignamanuel2@gmail.com>
2024-06-04 23:00:24 -0300

    hice cambios en el proyecto basandome ene el del profe

---
+++ src/main/java/com/example/tpGrupo4/helpers/ViewRouteHelper.java
@@ -0,0 +1,17 @@
+package com.example.tpGrupo4.helpers;
+
+public class ViewRouteHelper {
+	/**** Views ****/
+	//HOME
+	public final static String INDEX = "home/index";
+	public final static String HELLO = "home/hello";
+	
+	//USER
+	public final static String USER_LOGIN = "user/login";
+	public final static String USER_LOGOUT = "user/logout";
+	public final static String USER_REGISTER = "user/register";
+	
+	/**** Redirects ****/
+	public final static String ROUTE = "/index";
+	
+}
---
+++ src/main/java/com/example/tpGrupo4/repository/IUserRepository.java
@@ -0,0 +1,18 @@
+package com.example.tpGrupo4.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.tpGrupo4.entities.User;
+
+
+@Repository("userRepository")
+public interface IUserRepository extends JpaRepository<User, Serializable> {
+
+	@Query("SELECT u FROM User u JOIN FETCH u.userRoles WHERE u.username = (:username)")
+	public abstract User findByUsernameAndFetchUserRolesEagerly(@Param("username") String username);
+}
