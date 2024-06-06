6c338a953b2e46fc7d4cab33b1c1f14e07c431d6
LeanAranda <arandaleandro2003@gmail.com>
2024-06-05 22:34:55 -0300

    tiro reset

---
+++ src/main/java/com/unla/tpGrupo4/TpGrupo4Application.java
@@ -0,0 +1,13 @@
+package com.unla.tpGrupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGrupo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/tpGrupo4/TpGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.tpGrupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGrupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
